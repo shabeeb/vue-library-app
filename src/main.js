908c0bda1fe25b1a03566a561517104ab1580fb5
Shabeeb K <mail@shabeebk.com>
2022-04-02 15:23:17 +0530

    full file commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import myButton from "vue-component-library";
+import "vue-component-library/dist/lib.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(myButton);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
